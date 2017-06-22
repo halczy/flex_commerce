eec5a9319ad06c981670e01312196316a91438c9
Ziyang Chen <halczy@gmail.com>
2017-06-22 23:41:59 +0800

    Add sessions_helper#remember to set cookies and token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.regenerate_remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -2,12 +2,30 @@ require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
 
-  let(:user_1) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
 
   describe '#login' do
     it 'writes user id to session' do
-      helper.login(user_1)
-      expect(session[:user_id]).to eql(user_1.id)
+      helper.login(user)
+      expect(session[:user_id]).to eql(user.id)
+    end
+  end
+
+  describe '#remember' do
+    it 'sets user_id and remember_token in cookies' do
+      # See user_controller_spec. Cookies read/write is not supported here.
+    end
+
+    it 'refreshes remember_token' do
+      old_token = user.remember_token
+      helper.remember(user)
+      expect(old_token).not_to eql(user.remember_token)
+    end
+
+    it 'creates new remember_token' do
+      user.remember_token = nil
+      helper.remember(user)
+      expect(user.remember_token).not_to be_nil
     end
   end
 
