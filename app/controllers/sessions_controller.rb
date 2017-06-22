59266f40a65ea445fea5a07ae41f5229b1fe5cc0
Ziyang Chen <halczy@gmail.com>
2017-06-22 19:39:10 +0800

    Implement redirection of authenticated user in sessions#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,12 @@ class SessionsController < ApplicationController
     @title = 'Sign In'
   end
 
+  def create
+    user = User.find_by(email: params[:session][:ident]) ||
+           User.find_by(cell_number: params[:session][:ident])
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    end
+  end
+
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  context 'POST create' do
+    let(:user_1) { FactoryGirl.create(:user) }
+
+    it 'allow sign in through email' do
+      post :create, params: { session: { ident: user_1.email,
+                                         password: 'example' } }
+
+      expect(response).to redirect_to(user_path(user_1))
+    end
+  end
+
 end
