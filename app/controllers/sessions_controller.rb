8665a8e493f369ea8dab9fe8bae8b9c76d55551c
Ziyang Chen <halczy@gmail.com>
2017-06-22 20:00:08 +0800

    Implement sessions#new negative case spec and set user_id in session store

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:ident]) ||
            User.find_by(cell_number: params[:session][:ident])
     if user && user.authenticate(params[:session][:password])
+      helpers.login(user)
       redirect_to user
+    else
+      render :new
     end
   end
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,13 +11,35 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   context 'POST create' do
-    let(:user_1) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user) }
 
-    it 'allow sign in through email' do
-      post :create, params: { session: { ident: user_1.email,
+    it 'allows sign in through email' do
+      post :create, params: { session: { ident: user.email,
                                          password: 'example' } }
 
-      expect(response).to redirect_to(user_path(user_1))
+      expect(response).to redirect_to(user_path(user))
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it 'allows sign in through cell number' do
+      post :create, params: { session: { ident: user.cell_number,
+                                         password: 'example' } }
+      expect(response).to redirect_to(user_path(user))
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it 'renders new template again when invalid ident is provided' do
+      post :create, params: { session: { ident: 'wrong ident',
+                                         password: 'wrong pass' } }
+      expect(response).to render_template(:new)
+      expect(session[:user_id]).to be_nil
+    end
+
+    it 'renders new template again when incorrect password is provided' do
+      post :create, params: { session: { ident: user.email,
+                                         password: 'wrongpass' } }
+      expect(response).to render_template(:new)
+      expect(session[:user_id]).to be_nil
     end
   end
 
