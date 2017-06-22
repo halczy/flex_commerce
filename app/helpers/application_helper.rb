fc435389487f6b9a8592abf0ce8219ffb559ff7a
Ziyang Chen <halczy@gmail.com>
2017-06-22 17:43:10 +0800

    Implement page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     "active" if current_page?(path)
   end
 
+  def page_title
+    base_title = 'Flex Commerce'
+    if @title
+      "#{@title} | #{base_title}"
+    else
+      base_title
+    end
+  end
+
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,18 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, :type => :helper do
+
+  describe "#page_title" do
+    let(:base_title) { "Flex Commerce" }
+
+    it "returns the instance variable + base title" do
+      assign(:title, "Test Title")
+      expect(helper.page_title).to eql("Test Title | #{base_title}")
+    end
+
+    it 'returns only base title when no instance variable is provided' do
+      expect(helper.page_title).to eql("#{base_title}")
+    end
+  end
+
 end
