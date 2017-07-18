87ffa2e15f95152c572f69a7b59ede2b8505b14c
Ziyang Chen <halczy@gmail.com>
2017-07-18 19:29:39 +0800

    Implement products create and show action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < Admin::AdminController
+  before_action :set_product, except: [:index, :new, :create]
 
   def index
     @products = Product.all
@@ -7,4 +8,30 @@ class Admin::ProductsController < Admin::AdminController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "Successfully created a product."
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+  private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :tag_line, :sku, :introduction,
+                                      :description, :specification, :digital,
+                                      :strict_inventory, :price_market,
+                                      :price_member, :price_reward, :cost)
+    end
 end
--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -39,7 +39,39 @@ RSpec.describe Admin::ProductsController, type: :controller do
   end
 
   describe 'POST create' do
+    product_params = { product: { name: 'Name',
+                                 tag_line: 'Tag Line',
+                                 sku: 'SKU',
+                                 introduction: 'Introduction',
+                                 description: 'Description',
+                                 digital: false,
+                                 strict_inventory: false,
+                                 price_market: 12.35,
+                                 price_member: 12.34,
+                                 price_reward: 12.33,
+                                 cost: 10 } }
 
+    context 'with valid params' do
+      it 'creates product' do
+        post :create, params: product_params
+        expect(response).to redirect_to admin_product_path(Product.last)
+        expect(flash[:success]).to be_present
+      end
+    end
+
+    context 'with invalid params' do
+      it 'renders new tempalte' do
+        post :create, params: { product: { sku: 'sku' } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'GET show' do
+    it 'responses successfully' do
+      get :show, params: { id: product.id }
+      expect(assigns(:product)).to eq(product)
+    end
   end
 
 
