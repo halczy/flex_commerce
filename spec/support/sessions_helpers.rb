8b257995fec1fcda878fcd86a1fc7933f0931aad
Ziyang Chen <halczy@gmail.com>
2017-07-26 23:04:45 +0800

    Add acceptance test to categories

--- spec/support/sessions_helpers.rb
+++ spec/support/sessions_helpers.rb
@@ -2,6 +2,13 @@ module SessionsHelpers
   def signin_as(user)
     session[:user_id] = user.id
   end
+
+  def feature_signin_as(user)
+    visit signin_path
+    fill_in 'session[ident]', with: user.email
+    fill_in 'session[password]', with: 'example'
+    click_button 'Submit'
+  end
 end
 
 RSpec.configure do |c|
