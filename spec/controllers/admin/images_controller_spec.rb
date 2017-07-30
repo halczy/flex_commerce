5e859dd8f81decd02395db38fd603e50e28cef87
Ziyang Chen <halczy@gmail.com>
2017-07-30 18:23:38 +0800

    Add spec for image destroy action

--- spec/controllers/admin/images_controller_spec.rb
+++ spec/controllers/admin/images_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Admin::ImagesController, type: :controller do
 
   before { signin_as(admin) }
 
-  describe 'GET index' do
+  describe 'GET #index' do
     it 'returns a success response' do
       get :index
       expect(response).to be_success
@@ -15,13 +15,28 @@ RSpec.describe Admin::ImagesController, type: :controller do
     end
   end
 
-  describe 'GET show' do
+  describe 'GET #show' do
     it 'returns a success response with an image instance' do
       get :show, params: { id: image.id }
       expect(response).to be_success
       expect(response).to render_template(:show)
       expect(assigns(:image)).to eq(image)
       expect(assigns(:image_data)).not_to be_empty
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deletes the image' do
+      img_to_rm = image
+      expect {
+        delete :destroy, params: { id: img_to_rm.id }
+      }.to change(Image, :count).by(-1)
+    end
+
+    it 'redirects to the images index with flash message' do
+      delete :destroy, params: { id: image.id }
+      expect(response).to redirect_to(admin_images_path)
+      expect(flash[:success]).to be_present
     end
   end
 
