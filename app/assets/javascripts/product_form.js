f13ccf8b31a8cd74a4291a478bf9ebdd94003367
Ziyang Chen <halczy@gmail.com>
2017-07-30 18:55:13 +0800

    Implement AJAX image upload

--- app/assets/javascripts/product_form.js
+++ app/assets/javascripts/product_form.js
@@ -4,3 +4,56 @@ function add_fields(link, association, content) {
   $(link).prev().append(content.replace(regexp, new_id));
   return false;
 }
+
+(function() {
+  var createStorageKey, host, uploadAttachment;
+
+  document.addEventListener("trix-attachment-add", function(event) {
+    var attachment;
+    attachment = event.attachment;
+    if (attachment.file) {
+      return uploadAttachment(attachment);
+    }
+  });
+
+  host = "/admin/images.json";
+
+  uploadAttachment = function(attachment) {
+    var file, form, key, xhr;
+    file = attachment.file;
+    key = createStorageKey(file);
+    form = new FormData;
+    form.append("key", key);
+    form.append("Content-Type", file.type);
+    form.append("image[image]", file);
+    xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
+    xhr.open("POST", host, true);
+    xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+    xhr.upload.onprogress = function(event) {
+      var progress;
+      progress = event.loaded / event.total * 100;
+      return attachment.setUploadProgress(progress);
+    };
+    xhr.onload = function() {
+      var href, url;
+      if (xhr.status === 200) {
+        url = href = host + key;
+        return attachment.setAttributes({
+          url: xhr.response.url,
+          href: xhr.response.url
+        });
+      }
+    };
+    return xhr.send(form);
+  };
+
+  createStorageKey = function(file) {
+    var date, day, time;
+    date = new Date();
+    day = date.toISOString().slice(0, 10);
+    time = date.getTime();
+    return "tmp/" + day + "/" + time + "-" + file.name;
+  };
+
+}).call(this);
--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,11 +1,22 @@
 class Admin::ImagesController < Admin::AdminController
   # Filters
   before_action :set_image, only: [:show, :destroy]
+
   def index
     @images = Image.all
   end
 
   def show
+  end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      render json: { id: @image.id, url: @image.image_url }.to_json
+    else
+      render json: @image.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -20,6 +31,10 @@ class Admin::ImagesController < Admin::AdminController
     def set_image
       @image = Image.find(params[:id])
       @image_data = JSON.parse(@image.image_data) if @image.image_data
+    end
+
+    def image_params
+      params.require(:image).permit(:image)
     end
 
 end
