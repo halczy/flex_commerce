fc41d6e32e7698f204d89f732223cee8dc079965
Ziyang Chen <halczy@gmail.com>
2017-08-16 16:46:48 +0800

    Add sign out acceptance spec

--- spec/features/signin_signout_spec.rb
+++ spec/features/signin_signout_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe 'sign in as customer' do
-
   let(:customer) { FactoryGirl.create(:customer) }
 
+  before do
+    visit root_path
+    click_link 'Sign In'
+  end
+
   it 'allows sign in with email' do
-    visit signin_path
     fill_in "session[ident]", with: customer.email
     fill_in "session[password]", with: customer.password
     click_button 'Submit'
@@ -15,7 +18,6 @@ describe 'sign in as customer' do
   end
 
   it 'allows sign in with cell number' do
-    visit signin_path
     fill_in "session[ident]", with: customer.cell_number
     fill_in "session[password]", with: customer.password
     click_button 'Submit'
@@ -25,7 +27,6 @@ describe 'sign in as customer' do
   end
 
   it 'displays error messages when using invalid information' do
-    visit signin_path
     click_button 'Submit'
 
     expect(page).to have_css('.alert.alert-warning')
@@ -35,7 +36,6 @@ describe 'sign in as customer' do
 end
 
 describe 'sign in as admin' do
-
   let(:admin) { FactoryGirl.create(:admin) }
 
   it 'redirects to admin dashboard' do
@@ -48,4 +48,35 @@ describe 'sign in as admin' do
   end
 
   it 'has link to admin dashboard from frontpage'
+end
+
+describe 'sign out' do
+  let(:customer) { FactoryGirl.create(:customer) }
+  let(:admin)    { FactoryGirl.create(:admin) }
+
+  it 'signs out customer and return to the home page' do
+    visit signin_path
+    fill_in "session[ident]", with: customer.email
+    fill_in "session[password]", with: customer.password
+    click_button 'Submit'
+    visit root_path
+    click_link 'Sign Out'
+
+    expect(page.current_path).to eq(root_path)
+    expect(page).to have_css('.alert.alert-info')
+    expect(page).to have_link('Sign In')
+  end
+
+  it 'signs out admin and return to the home page' do
+    visit signin_path
+    fill_in "session[ident]", with: admin.email
+    fill_in "session[password]", with: admin.password
+    click_button 'Submit'
+    visit root_path
+    click_link 'Sign Out'
+
+    expect(page.current_path).to eq(root_path)
+    expect(page).to have_css('.alert.alert-info')
+    expect(page).to have_link('Sign Up')
+  end
 end
