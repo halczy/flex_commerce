8a915dc397df02ee19107cf283161f41aedf1511
Ziyang Chen <halczy@gmail.com>
2017-08-17 05:18:38 +0800

    Implement Russian Doll caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
       redirect_to root_url
     end
   end
-  
+
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
