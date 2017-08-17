86310ae283e3e4551e160cd3b9934446316ee059
Ziyang Chen <halczy@gmail.com>
2017-08-17 19:05:31 +0000

    Add product search service with category scope

--- app/services/product_search_service.rb
+++ app/services/product_search_service.rb
@@ -4,5 +4,11 @@ class ProductSearchService < SearchService
     build('name', 'tag_line')
     Product.where(where_clause, where_args).order(updated_at: :desc)
   end
+  
+  def search_in_category(category_id)
+    build('name', 'tag_line')
+    category = Category.find(category_id)
+    category.products.where(where_clause, where_args)
+  end
 
 end
--- spec/services/product_search_service_spec.rb
+++ spec/services/product_search_service_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe ProductSearchService, type: :model do
   describe '#quick_search' do
     before do
       @product_1 = FactoryGirl.create(:product, name: 'Blue Book',
-                                               tag_line: 'A tag line')
+                                                tag_line: 'A tag line')
       @product_2 = FactoryGirl.create(:product, name: 'Yellow Book',
-                                               tag_line: 'YATL')
+                                                tag_line: 'YATL')
     end
 
     it 'returns the matching product' do
@@ -28,6 +28,29 @@ RSpec.describe ProductSearchService, type: :model do
       search_run = ProductSearchService.new('Book')
       result = search_run.quick_search
       expect(result).to match_array([@product_1, @product_2])
+    end
+  end
+  
+  describe '#search_in_category' do
+    before do
+      @product_1 = FactoryGirl.create(:product, name: 'Red Car')
+      @product_2 = FactoryGirl.create(:product, name: 'Blue Car')
+      @product_3 = FactoryGirl.create(:product, name: 'White Car')
+      @category = FactoryGirl.create(:category)
+      Categorization.create(category: @category, product: @product_1)
+      Categorization.create(category: @category, product: @product_2)
+    end
+    
+    it 'returns matching products' do
+      search_run = ProductSearchService.new('Car')
+      result = search_run.search_in_category(@category.id)
+      expect(result).to match_array([@product_1, @product_2])
+    end
+    
+    it "returns matching product" do
+      search_run = ProductSearchService.new('Blue')
+      result = search_run.search_in_category(@category.id)
+      expect(result).to match_array([@product_2])      
     end
   end
 end
