c81805beb53a08da313ed196d48e3637cad59ec8
Ziyang Chen <halczy@gmail.com>
2017-08-17 17:30:19 +0800

    Add front page product display and navbar menu acceptance spec

---
+++ spec/features/navbar_categories_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+describe 'menu' do
+  before do
+    @cat_1 = FactoryGirl.create(:category, display_order: 0)
+    @cat_2 = FactoryGirl.create(:category, display_order: 2)
+    @cat_3 = FactoryGirl.create(:category, display_order: 1)
+  end
+
+  it 'displays parent categories on navbar' do
+    visit root_path
+    expect(page).to have_content(@cat_1.name)
+    expect(page).to have_content(@cat_2.name)
+    expect(page).to have_content(@cat_3.name)
+  end
+
+  it 'displays parent categories in order' do
+    visit root_path
+    expect(page).to have_content("#{@cat_1.name} #{@cat_3.name} #{@cat_2.name}")
+  end
+
+  context 'child categories' do
+    before do
+      @child_cat_1 = FactoryGirl.create(:category, parent_id: @cat_2, display_order: 2)
+      @child_cat_2 = FactoryGirl.create(:category, parent_id: @cat_2, display_order: 1)
+    end
+
+    it 'displays on dropdown' do
+      visit root_path
+
+      expect(page).to have_content(@child_cat_1.name)
+      expect(page).to have_content(@child_cat_2.name)
+    end
+
+    it 'dispalys in order' do
+      visit root_path
+
+      expect(page).to have_content(
+        "#{@child_cat_2.name} #{@cat_2.name} #{@child_cat_1.name}"
+      )
+    end
+  end
+end
+
+describe 'brand' do
+  before do
+    @brand_1 = FactoryGirl.create(:brand, display_order: 0)
+    @brand_2 = FactoryGirl.create(:brand, display_order: 2)
+    @brand_3 = FactoryGirl.create(:brand, display_order: 1)
+  end
+
+  it 'displays brands' do
+    visit root_path
+
+    expect(page).to have_content(@brand_1.name)
+    expect(page).to have_content(@brand_2.name)
+    expect(page).to have_content(@brand_3.name)
+  end
+
+  it 'displays brands in order' do
+    visit root_path
+
+    expect(page).to have_content(
+      "#{@brand_1.name} #{@brand_3.name} #{@brand_2.name}"
+    )
+  end
+end
--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -24,6 +24,31 @@ describe 'visit homepage as guest' do
       expect(page).to have_content('Sign In')
     end
   end
+end
 
+describe 'frontpage displays feature products' do
+  before do
+    feature_cat = FactoryGirl.create(:feature)
+    @product_1 = FactoryGirl.create(:product)
+    FactoryGirl.create(:image, imageable_type: 'Product', imageable_id: @product_1.id)
+    @product_2 = FactoryGirl.create(:product)
+    Categorization.create(category: feature_cat, product: @product_1)
+    Categorization.create(category: feature_cat, product: @product_2)
+    Image.create(title: "Placeholder Image",
+                 in_use: true,
+                 source_channel: 0,
+                 image: Rack::Test::UploadedFile.new(File.join(
+                          Rails.root, 'public', 'placeholder_img',
+                          'no-image-slide.png'), 'image/png'))
+  end
 
+  it 'shows feature products' do
+    visit root_path
+
+    expect(page).to have_content('Feature Products')
+    expect(page).to have_content(@product_1.name)
+    expect(page).to have_content(@product_2.name)
+    expect(page).to have_content('Detail', count: 2)
+    expect(page).to have_content('Add to Cart', count: 2)
+  end
 end
