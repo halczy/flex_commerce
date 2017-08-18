f09e988299caedf8ee71482250fbe91f18fd52d5
Ziyang Chen <halczy@gmail.com>
2017-08-18 16:59:34 +0000

    Refactor categories_controller#search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,11 +11,10 @@ class CategoriesController < ApplicationController
       search = ProductSearchService.new(search_term)
       if params[:current_category] == 1
         @search_run = search.search_in_category(params[:category_id])
-        @search_result = @search_run.page(params[:page]).per(6)
       else
         @search_run = search.quick_search
-        @search_result = @search_run.page(params[:page]).per(6)
       end
+      @search_result = @search_run.page(params[:page]).per(6)
     else
       flash.now[:warning] = "Please enter one or more search terms."
       render :search
