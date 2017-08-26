f3a6fa3faad39b284bfb9582f31392438623f6c4
Ziyang Chen <halczy@gmail.com>
2017-08-26 12:54:53 +0800

    Enable touch on inventory model for better caching

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,14 +1,14 @@
 class Inventory < ApplicationRecord
   # Relationships
-  belongs_to :user, optional: true
-  belongs_to :product
+  belongs_to :user, touch: true, optional: true
+  belongs_to :product, touch: true
 
   # Scope | Enum
   scope :available,     -> { where(status: 0) }
   scope :unavailable,   -> { where.not(status: 0) }
   scope :destroyable,   -> { where("status <= ?", 2) }
   scope :undestroyable, -> { where("status >= ?", 3) }
-  enum status: { unsold: 0, in_cart: 1, in_order: 2, in_checkout: 3, 
+  enum status: { unsold: 0, in_cart: 1, in_order: 2, in_checkout: 3,
                  sold: 4, returned: 5 }
 
 end
