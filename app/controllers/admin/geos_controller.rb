adad57366dbe26dfb2d854c8a2b352cb3f0c9648
Ziyang Chen <halczy@gmail.com>
2017-09-08 12:36:47 +0800

    Remove unnecessary order condition from geos_controller

--- app/controllers/admin/geos_controller.rb
+++ app/controllers/admin/geos_controller.rb
@@ -3,7 +3,6 @@ class Admin::GeosController < Admin::AdminController
   def index
     geo_filter = params[:geo_filter] ||= ""
     geos = Geo.try(geo_filter) || Geo.all
-    # geos = geos.order(id: :asc)
     @geos = geos.page params[:page]
   end
 
