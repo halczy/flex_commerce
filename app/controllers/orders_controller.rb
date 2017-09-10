b717f8683ac073ecc67d1f777cc8a0ff0935ef4f
Ziyang Chen <halczy@gmail.com>
2017-09-10 18:35:18 +0800

    Implement order controller create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,16 @@
 class OrdersController < UsersController
   # Filters
+  before_action :authenticate_user
   before_action :set_order, except: [:create, :index]
 
   def create
-
+    order = OrderService.new(cart_id: params[:cart_id]).create
+    if order
+      redirect_to select_shipping_order_path(order)
+    else
+      flash[:danger] = 'Unable to create order.'
+      redirect_to cart_path
+    end
   end
 
   def select_shipping
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -7,14 +7,54 @@ RSpec.describe OrdersController, type: :controller do
   let(:inventory) { FactoryGirl.create(:inventory) }
   let(:new_order) { FactoryGirl.create(:new_order) }
 
-  describe 'POST create' do
+  before do |example|
+    unless example.metadata[:skip_before]
+      signin_as customer
+    end
+  end
 
+  describe 'POST create' do
+    context 'with valid attributes' do
+      before do
+        @cart = FactoryGirl.create(:cart, user: customer)
+        3.times { @cart.inventories << FactoryGirl.create(:inventory, cart: @cart) }
+      end
+
+      it 'creates order and redirect to select shipping' do
+        expect {
+          post :create, params: { cart_id: @cart.id }
+        }.to change(Order, :count).by(1)
+        expect(response).to redirect_to(select_shipping_order_path(Order.last))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'flash error when cart is empty' do
+        empty_cart = FactoryGirl.create(:cart, user: customer)
+        post :create, params: { cart_id: empty_cart.id }
+        expect(flash[:danger]).to be_present
+        expect(response).to redirect_to(cart_path)
+      end
+
+      it 'redirect_to to sign in if not logged in', skip_before: true do
+        3.times { cart.inventories << FactoryGirl.create(:inventory, cart: cart) }
+        post :create, params: { cart_id: cart.id }
+        expect(response).to redirect_to(signin_path)
+      end
+
+      it 'redirect_to to cart_path if no params is cart_id provided' do
+        post :create, params: { }
+        expect(flash[:danger]).to be_present
+        expect(response).to redirect_to(cart_path)
+      end
+    end
   end
 
   describe 'GET select_shipping' do
     it 'response successfully' do
       get :select_shipping, params: { id: new_order.id }
       expect(response).to be_success
+      expect(response).to render_template(:select_shipping)
     end
   end
 
