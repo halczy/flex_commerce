7eb8af2e01eef07042cd6998f8b16594d2d7ac29
Ziyang Chen <halczy@gmail.com>
2017-09-17 19:29:33 +0800

    Add basic validations to payment class

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,14 @@
 class Payment < ApplicationRecord
   # Relationships
   belongs_to :order, optional: true
+
+  # Validations
+  monetize :amount_cents, numericality: { greater_than_or_equal_to: 0 }
+  validates_presence_of :processor, :variety, :status
+
+  # Enum
+  enum processor: { wallet: 0, alipay: 1 }
+  enum variety:   { charge: 0, refund: 1, transfer: 2 }
+  enum status:    { created: 0, client_confirmed: 1, processor_confirmed: 2 }
+
 end
