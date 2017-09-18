0fb15dc17196ad9a4326c3b0ad64b748790de698
Ziyang Chen <halczy@gmail.com>
2017-09-18 19:11:45 +0800

    Add payment factory with wallet, charge, created as default attributes

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :payment do
-    amount "MyString"
-    status 1
-    processor 1
-    variety 1
-    processor_response ""
-    order nil
+    association :order, factory: :order, confirmed: true
+    amount { order.total }
+    status 0
+    processor 0
+    variety 0
+    processor_response nil
   end
 end
