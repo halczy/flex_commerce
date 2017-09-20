13d24bacba9459d100f0314b8e587555524f1e91
Ziyang Chen <halczy@gmail.com>
2017-09-20 23:24:53 +0800

    Implement alipay return action in payments controller to confirm payment and order

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,3 +1,29 @@
 class PaymentsController < ApplicationController
+  before_action :verify_params, only: [ :alipay_return ]
 
+  def alipay_return
+    payment = Payment.find(params[:id])
+    payment.update(processor_response_return: request.query_parameters.to_json)
+    payment_service = PaymentService.new(payment_id:  payment.id)
+    if payment_service.alipay_confirm
+      redirect_to success_order_path(id: payment.order.id,
+                                     payment_id: payment.id)
+    else
+      flash[:danger] = 'Unable to confirm your order at the moment.
+                        Please contact customer service'
+      redirect_to root_url
+    end
+
+  end
+
+  private
+
+    def verify_params
+      alipay_client = helpers.create_alipay_client
+      unless alipay_client.verify?(request.query_parameters)
+        flash[:danger] = 'Invalid response from ALIPAY. Please contact customer
+                          service.'
+        redirect_to root_url
+      end
+    end
 end
--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -2,4 +2,81 @@ require 'rails_helper'
 
 RSpec.describe PaymentsController, type: :controller do
 
+  let(:payment_alipay)   { FactoryGirl.create(:payment, processor: 1) }
+
+  before do |example|
+    unless example.metadata[:skip_before]
+      allow_any_instance_of(Alipay::Client).to receive(:verify?).and_return(true)
+    end
+  end
+
+  describe 'GET alipay_return' do
+    context 'with success params' do
+      it 'saves returned params to payment_response' do
+        get :alipay_return, params: {
+          id: payment_alipay,
+          total_amount: payment_alipay.amount,
+          out_trade_no: payment_alipay.id
+        }
+
+        expect(payment_alipay.reload.processor_response_return).to be_present
+      end
+
+      it 'confirms payment and order' do
+        get :alipay_return, params: {
+          id: payment_alipay,
+          total_amount: payment_alipay.amount,
+          out_trade_no: payment_alipay.id
+        }
+        expect(payment_alipay.reload.client_side_confirmed?).to be_truthy
+        expect(payment_alipay.order.payment_success?).to be_truthy
+      end
+
+      it 'redirects to order success page' do
+        get :alipay_return, params: {
+          id: payment_alipay,
+          total_amount: payment_alipay.amount,
+          out_trade_no: payment_alipay.id
+        }
+
+        expect(response).to redirect_to(success_order_path(id: payment_alipay.order.id,
+                                                           payment_id: payment_alipay.id))
+      end
+    end
+
+    context 'with failure params' do
+      it 'rejects unsigned params', skip_before: true do
+        get :alipay_return, params: {
+          id: payment_alipay,
+          total_amount: payment_alipay.amount,
+          out_trade_no: payment_alipay.id
+        }
+        expect(flash[:danger]).to be_present
+        expect(response).to redirect_to root_url
+      end
+
+      it 'returns false if amount paid is incorrect' do
+        get :alipay_return, params: {
+          id: payment_alipay,
+          total_amount: '0.01',
+          out_trade_no: payment_alipay.id
+        }
+        expect(flash[:danger]).to be_present
+        expect(response).to redirect_to root_url
+      end
+
+      it 'returns false if payment id is incorrect' do
+        get :alipay_return, params: {
+          id: payment_alipay,
+          total_amount: payment_alipay.amount,
+          out_trade_no: '65432147'
+        }
+        expect(flash[:danger]).to be_present
+        expect(response).to redirect_to root_url
+      end
+    end
+  end
+
+  describe 'POST alipay_notify' do
+  end
 end
