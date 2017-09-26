e2eb1d2c7e2eddb85c249b699612a9b44f1ee2ec
Ziyang Chen <halczy@gmail.com>
2017-09-26 20:21:26 +0800

    Add tracking number to order model

--- db/migrate/0070_create_orders.rb
+++ db/migrate/0070_create_orders.rb
@@ -1,12 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders, id: :uuid do |t|
-      t.integer    :status,        index: true, default: 0
-      t.monetize   :shipping_cost, default: 0
-      t.references :user,          index: true, foreign_key: true, type: :uuid
+      t.integer    :status,          index: true, default: 0
+      t.monetize   :shipping_cost,   default: 0
+      t.string     :tracking_number
+      t.references :user,            index: true, foreign_key: true, type: :uuid
 
       t.timestamps
-
     end
 
     change_column :orders, :shipping_cost_cents, :integer, limit: 8
