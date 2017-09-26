14a7de3ada1f85df24c79b62b63ef9a2d2ec723a
Ziyang Chen <halczy@gmail.com>
2017-09-26 13:31:30 +0800

    Add random string to geo factories name to avoid triggering the unique name validation

--- spec/factories/geos.rb
+++ spec/factories/geos.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :country, class: Geo do
     id { Faker::Number.number }
-    name { Faker::Address.country }
+    name { Faker::Address.country + Faker::Number.number(2) }
     level 0
     parent nil
   end
@@ -15,28 +15,28 @@ FactoryGirl.define do
 
   factory :province, class: Geo do
     id { Faker::Number.number }
-    name { Faker::Address.state }
+    name { Faker::Address.country + Faker::Number.number(2) }
     level 1
     association :parent, factory: :country
   end
 
   factory :city, class: Geo do
     id { Faker::Number.number }
-    name { Faker::Address.city }
+    name { Faker::Address.country + Faker::Number.number(2) }
     level 2
     association :parent, factory: :province
   end
 
   factory :district, class: Geo do
     id { Faker::Number.number }
-    name { Faker::Address.community }
+    name { Faker::Address.country + Faker::Number.number(2) }
     level 3
     association :parent, factory: :city
   end
 
   factory :community, class: Geo do
     id { Faker::Number.number }
-    name { Faker::Address.community }
+    name { Faker::Address.country + Faker::Number.number(2) }
     level 4
     association :parent, factory: :district
   end
