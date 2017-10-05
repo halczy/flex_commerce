a337e16d613fdc29df24db6e5f6b159ecc449747
Ziyang Chen <halczy@gmail.com>
2017-10-06 00:53:05 +0800

    Implement admin app configs controller edit action

--- app/controllers/admin/application_configurations_controller.rb
+++ app/controllers/admin/application_configurations_controller.rb
@@ -1,7 +1,16 @@
 class Admin::ApplicationConfigurationsController < Admin::AdminController
+  before_action :set_app_config, except: [ :index ]
 
   def index
-    @app_configs = ApplicationConfiguration.all
+    @app_configs = ApplicationConfiguration.all.order(created_at: :asc)
   end
+
+  def edit; end
+
+  private
+
+    def set_app_config
+      @app_config = ApplicationConfiguration.find(params[:id])
+    end
 
 end
--- spec/controllers/admin/application_configurations_controller_spec.rb
+++ spec/controllers/admin/application_configurations_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Admin::ApplicationConfigurationsController, type: :controller do
 
   let(:admin) { FactoryGirl.create(:admin) }
+  let(:app_config) { ApplicationConfiguration.create(name: 'test_config',
+                                                     value: 'test_value') }
 
   before { signin_as admin }
 
@@ -11,6 +13,14 @@ RSpec.describe Admin::ApplicationConfigurationsController, type: :controller do
       get :index
       expect(response).to be_success
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET edit' do
+    it 'responses successfully' do
+      get :edit, params: { id: app_config.id }
+      expect(response).to be_success
+      expect(response).to render_template(:edit)
     end
   end
 end
