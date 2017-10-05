3b9c8e9fdb7cc568c443ca4b6203a64a6a5827c8
Ziyang Chen <halczy@gmail.com>
2017-10-06 01:07:48 +0800

    Implement admin app configs controller update action

--- app/controllers/admin/application_configurations_controller.rb
+++ app/controllers/admin/application_configurations_controller.rb
@@ -7,10 +7,24 @@ class Admin::ApplicationConfigurationsController < Admin::AdminController
 
   def edit; end
 
+  def update
+    if @app_config.update(app_config_params)
+      flash[:success] = "Successfully updated the configuration."
+      redirect_to admin_application_configurations_path
+    else
+      render :edit
+    end
+  end
+
   private
 
     def set_app_config
       @app_config = ApplicationConfiguration.find(params[:id])
+    end
+
+    def app_config_params
+      params.require(:application_configuration).permit(:name, :value,
+                                                        :plain, :status)
     end
 
 end
--- spec/controllers/admin/application_configurations_controller_spec.rb
+++ spec/controllers/admin/application_configurations_controller_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe Admin::ApplicationConfigurationsController, type: :controller do
       expect(response).to render_template(:edit)
     end
   end
+
+  describe 'PATCH update' do
+    it 'updates app config record with valid params' do
+      patch :update, params: {
+        id: app_config.id,
+        application_configuration: { value: 'new_value' }
+      }
+      expect(response).to redirect_to(admin_application_configurations_path)
+      expect(flash[:success]).to be_present
+      expect(app_config.reload.value).to eq('new_value')
+    end
+
+    it 'renders error messages with invalid params' do
+      patch :update, params: {
+        id: app_config.id,
+        application_configuration: { name: '' }
+      }
+      expect(response).to render_template(:edit)
+    end
+  end
 end
