56ee356109cd594e5853ed9d480ea18334073edd
Ziyang Chen <halczy@gmail.com>
2017-10-06 17:47:39 +0800

    Create transfer service

--- spec/factories/transfers.rb
+++ spec/factories/transfers.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :transfer do
     amount { Money.new(100) }
+    proceesor 'wallet'
     transferer { FactoryGirl.create(:customer) }
     transferee { FactoryGirl.create(:customer) }
   end
