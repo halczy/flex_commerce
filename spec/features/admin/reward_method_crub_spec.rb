550c9c9ab3ad2c6835d5f79f20bca4a4efc80f6e
Ziyang Chen <halczy@gmail.com>
2017-10-09 22:41:21 +0800

    Add acceptance spec for reward method edit process

--- spec/features/admin/reward_method_crub_spec.rb
+++ spec/features/admin/reward_method_crub_spec.rb
@@ -11,8 +11,8 @@ describe 'Reward Method CRUD', type: :feature do
     it 'can create referral type reward method' do
       visit admin_reward_methods_path
       click_on 'New Reward Method'
-      fill_in "reward_method[name]", with: "Feature Test"
-      fill_in "reward_method[percentage]", with: "12"
+      fill_in 'reward_method[name]', with: 'Feature Test'
+      fill_in 'reward_method[percentage]', with: '12'
       select 'Referral', from: 'reward_method[variety]'
       click_on 'Submit'
 
@@ -25,11 +25,37 @@ describe 'Reward Method CRUD', type: :feature do
     it 'displays error message when name is not filled' do
       visit admin_reward_methods_path
       click_on 'New Reward Method'
+      click_on 'Submit'
+
       expect(page).to have_css('#error_messages')
     end
   end
 
   describe 'edit' do
+    it 'can edit existing reward' do
+      ref_reward
+      visit admin_reward_methods_path
+      click_on 'Edit'
+      fill_in 'reward_method[name]', with: 'Feature Test'
+      fill_in 'reward_method[percentage]', with: '12'
+      select 'Referral', from: 'reward_method[variety]'
+      click_on 'Submit'
+
+      expect(page.current_path).to eq(admin_reward_method_path(RewardMethod.last))
+      expect(page).to have_content('Feature Test')
+      expect(page).to have_content('Percentage 12%')
+      expect(page).to have_content('Referral')
+    end
+
+    it 'displays error message when edited param is invalid' do
+      ref_reward
+      visit admin_reward_methods_path
+      click_on 'Edit'
+      fill_in 'reward_method[name]', with: ''
+      click_on 'Submit'
+
+      expect(page).to have_css('#error_messages')
+    end
   end
 
   describe 'destroy' do
