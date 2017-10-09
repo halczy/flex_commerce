166278a000cf0119167a1f83d44a6a4d8cf15c00
Ziyang Chen <halczy@gmail.com>
2017-10-09 18:14:29 +0800

    Implement admin reward method controller create action

--- app/controllers/admin/reward_methods_controller.rb
+++ app/controllers/admin/reward_methods_controller.rb
@@ -1,4 +1,5 @@
 class Admin::RewardMethodsController < Admin::AdminController
+  before_action :set_reward_method, except: [ :index, :new, :create ]
 
   def index
     @reward_methods = RewardMethod.all
@@ -8,4 +9,31 @@ class Admin::RewardMethodsController < Admin::AdminController
     @reward_method = RewardMethod.new
   end
 
+  def create
+    @reward_method = RewardMethod.new(reward_method_params)
+    if @reward_method.save
+      flash[:success] = "Successfully created a reward method."
+      set_settings
+      redirect_to admin_reward_method_path(@reward_method)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def set_reward_method
+      @reward_method = RewardMethod.find(params[:id])
+    end
+
+    def reward_method_params
+      params.require(:reward_method).permit(:name, :variety)
+    end
+
+    def set_settings
+      if @reward_method.referral? && params[:reward_method][:percentage]
+        @reward_method.settings[:percentage] = params[:reward_method][:percentage]
+      end
+      @reward_method.save
+    end
 end
--- spec/controllers/admin/reward_methods_controller_spec.rb
+++ spec/controllers/admin/reward_methods_controller_spec.rb
@@ -19,4 +19,38 @@ RSpec.describe Admin::RewardMethodsController, type: :controller do
       expect(response).to render_template(:new)
     end
   end
+
+  describe 'POST create' do
+    context 'with valid params' do
+      let(:valid_ref_attrs) {{
+        name: 'Referral Reward',
+        variety: 'referral',
+        percentage: '10'
+      }}
+
+      it 'creates a reward method' do
+        expect {
+          post :create, params: { reward_method: valid_ref_attrs }
+        }.to change(RewardMethod, :count).by(1)
+      end
+
+      it 'sets percentage attribute for referral type method' do
+        post :create, params: { reward_method: valid_ref_attrs }
+        expect(RewardMethod.last.reload.percentage).to eq('10')
+      end
+
+      it 'redirects to reward method show action' do
+        post :create, params: { reward_method: valid_ref_attrs }
+        expect(flash[:success]).to be_present
+        expect(response).to redirect_to(admin_reward_method_path RewardMethod.last)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'renders error messages when params is invalid' do
+        post :create, params: { reward_method: { name: '' } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
