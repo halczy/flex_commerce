d799d50bf52bb4823d8f26ccae9d1d3129f0db32
Ziyang Chen <halczy@gmail.com>
2017-10-11 01:54:33 +0800

    Add #reward_amount and #referral_reward_amount to reward service

--- app/services/reward_service.rb
+++ app/services/reward_service.rb
@@ -1,12 +1,33 @@
 class RewardService
-  attr_accessor :order
+  attr_accessor :order, :referral_amount
 
   def initialize(order_id: nil)
     @order = Order.find(order_id)
+    @referral_amount = Money.new(0)
   end
 
   def distribute
-
+    # CALCUATE REWARD AMOUNTS FOR ALL REWARD METHODS
+    # CALL PRESET SUB DISTRIBUTE METHODS
   end
+
+  def distribute_referral
+    # GET REFERER
+    # DEPOSIT VIA PAYMENT SERVICE
+  end
+
+  def reward_amount(inv, reward_method)
+    case reward_method.variety
+    when 'referral' then referral_reward_amount_(inv, reward_method)
+    end
+  end
+
+  private
+
+    def referral_reward_amount(inv, reward_method)
+      percentage = reward_method.reload.percentage.to_f * 0.01
+      @referral_amount += inv.product.price_reward * percentage
+    end
+
 
 end
--- spec/services/reward_service_spec.rb
+++ spec/services/reward_service_spec.rb
@@ -6,12 +6,25 @@ RSpec.describe RewardService, type: :model do
     @ref_reward = FactoryGirl.create(:ref_reward, no_products: true)
     @success_order = FactoryGirl.create(:payment_order, success: true)
     @success_order.products.each { |p| p.reward_methods << @ref_reward }
+    @reward_service = RewardService.new(order_id: @success_order.id)
   end
 
   describe '#initialize' do
     it 'initializes with order id' do
       reward_service = RewardService.new(order_id: @success_order.id)
       expect(reward_service.order).to eq(@success_order)
+      expect(reward_service.referral_amount).to eq(0)
+    end
+  end
+
+  describe '#reward_amount' do
+    describe '#referral_reward_amount' do
+      it 'returns the referral reward amount' do
+        inv = @success_order.inventories.sample
+        ref_amount = inv.product.price_reward * 0.05
+        @reward_service.send(:referral_reward_amount, inv, @ref_reward)
+        expect(@reward_service.referral_amount).to eq(ref_amount)
+      end
     end
   end
 
