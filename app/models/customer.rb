ea66e71f11ebce87acd885c9a56e8494772a048f
Ziyang Chen <halczy@gmail.com>
2017-10-11 16:19:07 +0800

    Add #total_spent to customer model calcuate amount spent by customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < User
+
+  def total_spent
+    qualified_orders = Order.where(user: self).where("status >= 60")
+    qualified_orders.sum { |order| order.total }
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
 
-  let(:customer) { FactoryGirl.create(:customer) }
+  let(:customer)        { FactoryGirl.create(:customer) }
+  let(:completed_order) { FactoryGirl.create(:service_order, completed: true) }
 
   describe 'creation' do
     it 'can be created' do
@@ -14,6 +15,16 @@ RSpec.describe Customer, type: :model do
   describe '#customer?' do
     it 'returns true when user is an admin' do
       expect(customer.customer?).to be_truthy
+    end
+  end
+
+  describe '#total_spent' do
+    it 'sums up customer payment success orders total ' do
+      qo_1 = FactoryGirl.create(:service_order, user: customer, completed: true)
+      qo_2 = FactoryGirl.create(:payment_order, user: customer, success: true)
+      FactoryGirl.create(:order, user: customer)
+      exp_total = qo_1.total + qo_2.total
+      expect(customer.total_spent).to eq(exp_total)
     end
   end
 
