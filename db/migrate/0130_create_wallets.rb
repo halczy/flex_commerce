053f8a0be9cc896916f2509d40c6b192b30da07c
Ziyang Chen <halczy@gmail.com>
2017-10-11 11:26:01 +0800

    Add withdrawable column to wallet table

--- db/migrate/0130_create_wallets.rb
+++ db/migrate/0130_create_wallets.rb
@@ -1,15 +1,16 @@
 class CreateWallets < ActiveRecord::Migration[5.1]
   def change
     create_table :wallets, id: :uuid do |t|
-      t.monetize   :balance, default: 0
-      t.monetize   :pending, default: 0
+      t.monetize   :balance,      default: 0
+      t.monetize   :pending,      default: 0
+      t.monetize   :withdrawable, default: 0
       t.references :user,    index: true, type: :uuid, foreign_key: true
 
       t.timestamps
-
     end
 
-    change_column :wallets, :balance_cents, :integer, limit: 8
-    change_column :wallets, :pending_cents, :integer, limit: 8
+    change_column :wallets, :balance_cents,      :integer, limit: 8
+    change_column :wallets, :pending_cents,      :integer, limit: 8
+    change_column :wallets, :withdrawable_cents, :integer, limit: 8
   end
 end
