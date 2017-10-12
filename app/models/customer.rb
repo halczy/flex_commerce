7595d2f972a3c276d3ebefbdda170906297288e9
Ziyang Chen <halczy@gmail.com>
2017-10-12 19:21:40 +0800

    Add #reward_income tally method to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,14 @@
 class Customer < User
-
   def total_spent
     qualified_orders = Order.where(user: self).where("status >= 60")
     qualified_orders.sum { |order| order.total }
+  end
+
+  def reward_income
+    rewards = Transaction.all.select do |t|
+      t.originable.try("reward?") &&
+      t.processable == wallet
+    end
+    rewards.sum { |t| t.amount }
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Customer, type: :model do
   end
 
   describe '#total_spent' do
-    it 'sums up customer payment success orders total ' do
+    it 'sums up customer payment success orders total' do
       qo_1 = FactoryGirl.create(:service_order, user: customer, completed: true)
       qo_2 = FactoryGirl.create(:payment_order, user: customer, success: true)
       FactoryGirl.create(:order, user: customer)
@@ -28,4 +28,19 @@ RSpec.describe Customer, type: :model do
     end
   end
 
+  describe '#reward_income' do
+    it 'sums up customer reward amount' do
+      3.times do
+        payment_service = PaymentService.new(
+          order_id: FactoryGirl.create(:service_order, completed: true).id,
+          amount: 150.to_money,
+          user_id: customer.id,
+          variety: 'reward'
+        )
+        payment_service.create
+        payment_service.reward
+      end
+      expect(customer.reward_income).to eq(450.to_money)
+    end
+  end
 end
