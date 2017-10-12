63663cf7c99bb47db73b52b43a15a06f28d8e841
Ziyang Chen <halczy@gmail.com>
2017-10-12 16:15:27 +0800

    Refactor transfer service to accommodate more transfer processors

--- app/services/transfer_service.rb
+++ app/services/transfer_service.rb
@@ -2,20 +2,29 @@ class TransferService
   attr_accessor :transfer, :transferer, :transferee, :amount,
                 :fund_source, :fund_target, :processor, :status
 
-  def initialize(transfer_id: nil, transferer_id: nil, transferee_id: nil, amount: nil)
+  def initialize(transfer_id: nil, transferer_id: nil, transferee_id: nil,
+                 amount: nil, processor: nil)
     @transfer = Transfer.find_by(id: transfer_id)
     @transferer = @transfer.try(:transferer) || User.find_by(id: transferer_id)
     @transferee = @transfer.try(:transferee) || User.find_by(id: transferee_id)
     @amount = @transfer.try(:amount) || Money.new(amount.to_f * 100)
+    @processor = processor || 'wallet'
   end
 
-  def create(processor: nil, fund_source: nil, fund_target: nil)
+  def create(fund_source: nil, fund_target: nil)
+    case @processor
+    when 'wallet'
+      create_wallet_transfer(fund_source: fund_source, fund_target: fund_target)
+    end
+  end
+
+  def create_wallet_transfer(fund_source: nil, fund_target: nil)
     begin
       @transfer = Transfer.create(
         transferer: @transferer,
         transferee: @transferee,
         amount: @amount,
-        processor: (processor || 'wallet'),
+        processor: 'wallet',
         fund_source: (fund_source || @transferer.wallet),
         fund_target: (fund_target || @transferee.wallet)
       )
