992ba30ef37ac1f1fc45ecf20cab95562cdf3835
Ziyang Chen <halczy@gmail.com>
2017-10-16 14:15:30 +0800

    Implement admin dashboard to display orders pending action

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,10 @@
 class Admin::DashboardController < Admin::AdminController
 
   def index
-    @title = 'Dashboard'
+    @to_confirm_orders = Order.payment_success
+    @to_ship_orders = Order.staff_confirmed
+    # PENDING TRANSFER
+    # OOS Product
   end
 
 end
