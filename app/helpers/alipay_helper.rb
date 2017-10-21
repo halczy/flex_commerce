2857695a5a94077a989e8432ea1dadbda212b285
Ziyang Chen <halczy@gmail.com>
2017-10-22 00:45:25 +0800

    Enable AlipayHelper to get configs from either ENV or App.Config class

--- app/helpers/alipay_helper.rb
+++ app/helpers/alipay_helper.rb
@@ -1,10 +1,23 @@
 module AlipayHelper
 
   def create_alipay_client
-    Alipay::Client.new(url: ENV['ALIPAY_GATEWAY'],
-                       app_id: ENV['ALIPAY_APP_ID'],
-                       app_private_key: ENV['ALIPAY_APP_PRIVATE_KEY'],
-                       alipay_public_key: ENV['ALIPAY_PUBLIC_KEY'])
+    get_alipay_params
+    Alipay::Client.new(
+      url: @gateway,
+      app_id: @app_id,
+      app_private_key: @private_key,
+      alipay_public_key: @public_key
+    )
   end
 
+  def get_alipay_params
+    @gateway =     ApplicationConfiguration.get('alipay_gateway') ||
+                   ENV['ALIPAY_GATEWAY']
+    @app_id =      ApplicationConfiguration.get('alipay_app_id') ||
+                   ENV['ALIPAY_APP_ID']
+    @private_key = ApplicationConfiguration.get('alipay_app_private_key') ||
+                   ENV['ALIPAY_APP_PRIVATE_KEY']
+    @public_key  = ApplicationConfiguration.get('alipay_public_key') ||
+                   ENV['ALIPAY_PUBLIC_KEY']
+  end
 end
--- spec/helpers/alipay_helper_spec.rb
+++ spec/helpers/alipay_helper_spec.rb
@@ -6,6 +6,34 @@ RSpec.describe AlipayHelper, type: :helper do
     it 'creates an alipay client' do
       expect(helper.create_alipay_client).to be_an_instance_of Alipay::Client
     end
+
+    it 'creates an alipay client with correct params' do
+      ENV['ALIPAY_GATEWAY'] = 'https://example.com'
+      ApplicationConfiguration.create(name: 'alipay_app_id', plain: '4321')
+      key = OpenSSL::PKey::RSA.new(2048)
+      ApplicationConfiguration.create(name: 'alipay_app_private_key', value: key.to_s)
+      ApplicationConfiguration.create(name: 'alipay_public_key', value: key.public_key.to_s)
+      client = create_alipay_client
+
+      expect(client.instance_values['url']).to eq('https://example.com')
+      expect(client.instance_values['app_id']).to eq('4321')
+      expect(client.instance_values['app_private_key']).to eq(key.to_s)
+      expect(client.instance_values['alipay_public_key']).to eq(key.public_key.to_s)
+    end
+  end
+
+  describe 'get_alipay_params' do
+    it 'gets params from environment variable' do
+      ENV['ALIPAY_APP_ID'] = '1234'
+      get_alipay_params
+      expect(@app_id).to eq('1234')
+    end
+
+    it 'gets params from app configs' do
+      ApplicationConfiguration.create(name: 'alipay_gateway', value: 'abc.com')
+      get_alipay_params
+      expect(@gateway).to eq('abc.com')
+    end
   end
 
 end
